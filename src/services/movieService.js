6183fd760f67ec431dd1b901f62e415dd156133c
Haritha <harithashilan@gmail.com>
2026-05-27 18:53:19 +0530

    refactor: add axios instance and centralized api client

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,23 +1,24 @@
-import axios from 'axios';
+
 import {  getMovieDetailsUrl, getPopularMoviesUrl, getSearchMovieUrl } from '../utils/apiUrls'
+import apiClient from './apiClient';
 
 
 const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
 export const searchMovieByName = async (name) => {
     const url = getSearchMovieUrl(apiKey, name);
-    const response = await axios.get(url);
+    const response = await apiClient.get(url);
     return response.data.results;
 }
 
 export const getPopularMovies = async () => {
     const url = getPopularMoviesUrl(apiKey);
-    var response = await axios.get(url);
+    var response = await apiClient.get(url);
     return response.data.results;
 }
 
 export const getMovieDetailsById = async (id) => {
     const url = getMovieDetailsUrl(apiKey, id);
-    var response = await axios.get(url);
+    var response = await apiClient.get(url);
     return response.data;
 }
